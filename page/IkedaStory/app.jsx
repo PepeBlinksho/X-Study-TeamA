345be22758852ff4b044d7c39cc7cf688f3135b4
sikeda <sikeda@FMX-P20210403.local>
2022-02-22 19:00:05 +0900

    セリフの番号をstateで管理

--- page/IkedaStory/app.jsx
+++ page/IkedaStory/app.jsx
@@ -4,39 +4,66 @@ import { GiftedChat } from 'react-native-gifted-chat'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import Icon2 from '../../assets/maria.jpeg'
 
-const AI = ['3150!']
+const AI = '3150!'
 
 const messageList = {
   0: {
-    0: 'おはよう',
-    1: 'こんばんわ',
+    0: '1: やらかしちゃいました >_<',
+    1: '2: 大変申し訳ありません -_-',
+    2: 'あなたは仕事でやらかしてしまいました。なんと返信する？'
   },
   1: {
+    0: '1: メールをご送信してしまいました。',
+    1: '2: 眠いです',
+    2: 'どうした。'
+  },
+  2: {
+    0: 'たまにはしょうがないですよね',
+    1: '次から気をつけます',
+    2: 'それは仕方ないな。謝りを入れておいて。'
+  },
+  3: {
     0: 'お疲れ様です',
     1: 'さようなら',
+    2: '今日はもういいよ'
   },
 }
 
 export default class App extends React.Component {
-
   constructor(props) {
     super(props)
     this.state = {
       messages: [],
+      number: 0,
     }
   }
 
   reply() {
-    return {
-      _id: Math.round(Math.random() * 100000000),
-      text: messageList[1][0],
-      createdAt: new Date(),
-      user: {
-        _id: 2,
-        name: '長澤まさみ',
-        avatar: Icon2,
-      },
-    }
+    return (
+      {
+        _id: Math.round(Math.random() * 100000000),
+        text: messageList[this.state.number][2],
+        createdAt: new Date(),
+        quickReplies: {
+          type: 'radio',
+          values: [
+            {
+              title: messageList[this.state.number][0],
+              value: '1',
+            },
+            {
+              title: messageList[this.state.number][1],
+              value: '0',
+            },
+          ],
+        },
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: Icon2,
+        },
+      }
+    )
   }
 
   onSend(messages = []) {
@@ -45,6 +72,7 @@ export default class App extends React.Component {
         GiftedChat.append(previousState.messages, messages),
         this.reply()
       ),
+      number: this.state.number + 1
     }))
   }
 
